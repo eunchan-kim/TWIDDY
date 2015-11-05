0ae771e279352643023d98cd1ad7ac5e1137974f
JuHyeng Han <sparkly9399@kaist.ac.kr>
2015-11-05 16:44:49 +0900

    디스플레이 감정표현에 애니메이션 효과 추가

--- TWIDDY_UI/src/com/example/twiddy_ui/DisplayEmotion.java
+++ TWIDDY_UI/src/com/example/twiddy_ui/DisplayEmotion.java
@@ -9,8 +9,12 @@ import android.graphics.Paint.Style;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.Transformation;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
@@ -22,13 +26,16 @@ public class DisplayEmotion  extends Activity implements OnClickListener{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display);
-		
-		Button btn_smile = (Button)findViewById(R.id.btn_smile);
+
+		Button btn_normal = (Button)findViewById(R.id.btn_normal);
+		Button btn_happy = (Button)findViewById(R.id.btn_happy);
 		Button btn_angry = (Button)findViewById(R.id.btn_angry);
-		btn_smile.setOnClickListener(this);
+		btn_normal.setOnClickListener(this);
+		btn_happy.setOnClickListener(this);
 		btn_angry.setOnClickListener(this);
 		
-		emotion = new Emotion(this);		
+		emotion = new Emotion(this);
+		emotion.changeEmotion(EnumEmotion.Normal);
 		FrameLayout frame = (FrameLayout)findViewById(R.id.layout_display);
 		frame.addView(emotion, 0);
 	}
@@ -38,7 +45,10 @@ public class DisplayEmotion  extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		switch(v.getId())
 		{
-		case R.id.btn_smile:
+		case R.id.btn_normal:
+			emotion.changeEmotion(EnumEmotion.Normal);
+			break;
+		case R.id.btn_happy:
 			emotion.changeEmotion(EnumEmotion.Happy);
 			break;
 		case R.id.btn_angry:
@@ -53,10 +63,12 @@ public class DisplayEmotion  extends Activity implements OnClickListener{
 		Paint pnt;
 		DisplayMetrics displayMetrics;
 		int deviceWidth, deviceHeight;
-		int smileStart, angle, angryStart;
+		float startAngle;
+		float mouseTop, mouseBottom;
 		int radius, eyeRadius;
 		RectF rect;
 		EnumEmotion currentEmotion;
+		EmotionAnimation eAnim;
 		
 		public Emotion(Context context) {
 			super(context);
@@ -69,16 +81,19 @@ public class DisplayEmotion  extends Activity implements OnClickListener{
             radius = deviceWidth/4;
             eyeRadius = radius/6;
             rect = new RectF();
-            currentEmotion = EnumEmotion.Normal;
-            smileStart = 30;
-            angryStart = 210;
-            angle = 120;    
+            currentEmotion = EnumEmotion.Normal; //Default: Normal
+            startAngle = 30f;
+            mouseTop = deviceHeight/5*2+eyeRadius*3;
+            mouseBottom = deviceHeight/5*2+eyeRadius*3;
+            eAnim = new EmotionAnimation(currentEmotion);
             postInvalidate();
 		}
 		
 		public void changeEmotion(EnumEmotion e)
 		{
 			currentEmotion = e;
+			eAnim.changeEmotion(e);
+			this.startAnimation(eAnim);
 			postInvalidate();
 		}
 		
@@ -86,6 +101,7 @@ public class DisplayEmotion  extends Activity implements OnClickListener{
 		protected void onDraw(Canvas canvas) {
 			// TODO Auto-generated method stub
 			super.onDraw(canvas);
+			
             switch(currentEmotion)
             {
             case Normal:
@@ -108,21 +124,95 @@ public class DisplayEmotion  extends Activity implements OnClickListener{
             pnt.setStrokeWidth(10);
             pnt.setStyle(Style.STROKE);
             
-            switch(currentEmotion)
-            {
-            case Normal:
-            	canvas.drawLine(deviceWidth/2-eyeRadius*3, deviceHeight/5*2+eyeRadius*3, 
-            			deviceWidth/2+eyeRadius*3, deviceHeight/5*2+eyeRadius*3, pnt);
-            	break;
-            case Happy:
-            	rect.set(deviceWidth/2-eyeRadius*3,deviceHeight/5*2,deviceWidth/2+eyeRadius*3,deviceHeight/5*2+eyeRadius*3);
-                canvas.drawArc(rect, smileStart, angle, false, pnt);
-                break;
-            case Angry:
-            	rect.set(deviceWidth/2-eyeRadius*3,deviceHeight/5*2+eyeRadius*3,deviceWidth/2+eyeRadius*3,deviceHeight/5*2+eyeRadius*6);
-                canvas.drawArc(rect, angryStart, angle, false, pnt);
-                break;
-            }            
-		}	
+            rect.set(deviceWidth/2-eyeRadius*3,mouseTop,deviceWidth/2+eyeRadius*3,mouseBottom);                            
+            canvas.drawArc(rect, startAngle, 120, false, pnt);
+   
+		}
+		
+		protected class EmotionAnimation extends Animation {
+			EnumEmotion mEmotion;
+			EnumEmotion prevEmotion;
+
+			float normalStart = 30f;
+			float happyStart = 30f;
+			float angryStart = 210f;
+
+			float normalTop = deviceHeight/5*2+eyeRadius*3;
+			float happyTop = deviceHeight/5*2;
+			float angryTop = deviceHeight/5*2+eyeRadius*3;
+
+			float normalBottom = deviceHeight/5*2+eyeRadius*3;
+			float happyBottom = deviceHeight/5*2+eyeRadius*3;
+			float angryBottom = deviceHeight/5*2+eyeRadius*6;
+			
+			float curTop;
+			float curBottom;
+			
+			public EmotionAnimation(EnumEmotion etype) {
+				mEmotion = etype;
+				
+				setDuration(1000);
+				setInterpolator(new LinearInterpolator());
+			}
+			
+			public void changeEmotion(EnumEmotion etype) {
+				prevEmotion = mEmotion;
+				curTop = emotion.mouseTop;
+				curBottom = emotion.mouseBottom;
+				mEmotion = etype;
+			}
+
+			@Override
+			protected void applyTransformation(float interpolatedTime, Transformation t) {
+				// TODO Auto-generated method stub
+				float fullTime = interpolatedTime * 2;
+				
+	            switch(mEmotion)
+	            {
+	            case Normal:
+	            	if(fullTime < 1.f) {
+		            	emotion.mouseTop = curTop * (1-fullTime) + normalTop * fullTime;
+		            	emotion.mouseBottom = curBottom * (1-fullTime) + normalBottom * fullTime;
+	            	}	            	        	
+	            	break;
+	            case Happy:
+	            	if(prevEmotion == EnumEmotion.Normal) {
+	            		if(fullTime < 1.f) {
+		            		emotion.mouseTop = curTop * (1-fullTime) + happyTop * fullTime;
+			            	emotion.startAngle = happyStart;		
+	            		}
+	            	}
+	            	else {
+	            		if(fullTime < 1.f) {
+			            	emotion.mouseBottom = curBottom * (1-fullTime) + happyBottom * fullTime;
+		            	}
+		            	else {
+		            		emotion.mouseTop = curTop * (2-fullTime) + happyTop * (fullTime-1);
+			            	emotion.startAngle = happyStart;
+		            	}
+	            	}	            	
+	                break;
+	            case Angry:
+		        	if(prevEmotion == EnumEmotion.Normal) {
+		        		if(fullTime < 1.f) {
+			        		emotion.mouseBottom = curBottom * (1-fullTime) + angryBottom * fullTime;
+			            	emotion.startAngle = angryStart;
+		        		}
+	            	}
+	            	else {
+		            	if(fullTime < 1.f) {
+			            	emotion.mouseTop = curTop * (1-fullTime) + angryTop * fullTime;
+		            	}
+		            	else {
+		            		emotion.mouseBottom = curBottom * (2-fullTime) + angryBottom * (fullTime-1);
+			            	emotion.startAngle = angryStart;
+		            	}
+	            	}	
+	                break;
+	            }   
+	            invalidate();
+			}
+			
+		}
 	}
 }
