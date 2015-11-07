4e4e408123836e4748783aebaf52ab76aefef209
Yeonni <yeonhee.ryou@gmail.com>
2015-11-07 14:51:22 +0900

    tmp STT module

--- TWIDDY_UI/src/com/example/twiddy_ui/TTSActivity.java
+++ TWIDDY_UI/src/com/example/twiddy_ui/TTSActivity.java
@@ -1,5 +1,7 @@
 package com.example.twiddy_ui;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,15 +9,21 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import net.daum.mf.speech.api.SpeechRecognizeListener;
+import net.daum.mf.speech.api.SpeechRecognizerClient;
+import net.daum.mf.speech.api.SpeechRecognizerManager;
 import net.daum.mf.speech.api.TextToSpeechClient;
 import net.daum.mf.speech.api.TextToSpeechListener;
 import net.daum.mf.speech.api.TextToSpeechManager;
 
 
-public class TTSActivity extends Activity implements OnClickListener, TextToSpeechListener{
-	private TextToSpeechClient tts_client;
+public class TTSActivity extends Activity implements OnClickListener, TextToSpeechListener, SpeechRecognizeListener{
 	public static String NEWTONE_API_KEY = "dcd2a896fab93d17a09e2d752ef0e145";
-	
+	private TextToSpeechClient tts_client;
+	private SpeechRecognizerClient stt_client;
+	private String result_text = "Ready?";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,6 +40,16 @@ public class TTSActivity extends Activity implements OnClickListener, TextToSpee
 
 		Button btn_read = (Button) findViewById(R.id.btn_read);
 		btn_read.setOnClickListener(this);
+
+		SpeechRecognizerManager.getInstance().initializeLibrary(this);
+		stt_client = new SpeechRecognizerClient.Builder()
+				.setApiKey(NEWTONE_API_KEY)
+				.setServiceType(SpeechRecognizerClient.SERVICE_TYPE_WEB)
+				.build();
+		stt_client.setSpeechRecognizeListener(this);
+
+		Button btn_hear = (Button) findViewById(R.id.btn_hear);
+		btn_hear.setOnClickListener(this);		
 	}
 
 	@Override
@@ -41,26 +59,75 @@ public class TTSActivity extends Activity implements OnClickListener, TextToSpee
 		case R.id.btn_read:
 			EditText content_view = (EditText) findViewById(R.id.tts_content);
 			String content_text = content_view.getText().toString();
-			Log.d("msg", content_text);
 			this.tts_client.play(content_text);
 			break;
+
+		case R.id.btn_hear:
+			this.stt_client.startRecording(true);
+			break;
 		}
+	}
+	
+	private void showReuslt() {
+		TextView result_view = (TextView) findViewById(R.id.stt_result);
+		result_view.setText(this.result_text);
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		TextToSpeechManager.getInstance().finalizeLibrary();
+		SpeechRecognizerManager.getInstance().finalizeLibrary();
 	}
 
+	/* TextToSpeechListener Methods */
 	@Override
 	public void onError(int arg0, String arg1) {
-		Log.e(String.format("Play error %d", arg0), arg1);
+		Log.e("Play error " + arg0, arg1);
 	}
 
 	@Override
 	public void onFinished() {
 		// TODO Auto-generated method stub	
+	}
+
+	/* SpeechRecognizeListener Methods */
+	@Override
+	public void onAudioLevel(float arg0) {
+//		Log.d("speech", "onAudioLevel: " + arg0);
+	}
+
+	@Override
+	public void onBeginningOfSpeech() {
+		Log.d("speech", "onBeginningOfSpeech");
+	}
+
+	@Override
+	public void onEndOfSpeech() {
+		Log.d("speech", "onEndOfSpeech");
+	}
+
+	@Override
+	public void onPartialResult(String arg0) {
+		Log.d("speech", "onPartialResult: " + arg0);
+		
+	}
+
+	@Override
+	public void onReady() {
+		Log.d("speech", "onReady");
+	}
+
+	@Override
+	public void onResults(Bundle arg0) {
+		String res = "NONE";
+		ArrayList<String> texts = arg0.getStringArrayList(SpeechRecognizerClient.KEY_RECOGNITION_RESULTS);
+
+		if (texts.size() > 0) {
+			this.result_text = texts.get(0);
+			res = texts.get(0);
+		}
+		Log.d("speech", "onResults: " + res);
 	}
 
 }
