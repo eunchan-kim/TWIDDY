d6e5cca2fca137344d2fca211204f1106900e5a3
Eunchan <pavian@kaist.ac.kr>
2015-11-13 15:22:58 +0900

    Changed json rpc library due to jvm-thread problem of previous library

--- emotion_extractor/client.py
+++ emotion_extractor/client.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
+import requests
+import json
 
-import pyjsonrpc
+def main():
+    url = "http://143.248.142.86:4000/jsonrpc"
+    headers = {'content-type': 'application/json'}
 
-http_client = pyjsonrpc.HttpClient("http://localhost:10002", gzipped = True, debug = True)
+    payload = {
+        "method": "get_emotion",
+        "params": [u"행복행복행복"],
+        "jsonrpc": "2.0",
+        "id": 0,
+    }
+    response = requests.post(
+        url, data=json.dumps(payload), headers=headers).json()
+    print response
+    print response["result"]
 
-# two kinds of method calling is possible.
-# 1) http_client.call(method name, ...)
-# 2) http_client.method_name(...)
-print http_client.call("get_emotion", u"오늘은 너무나도 행복행복해요")
-print http_client.get_emotion(u"헬로헬로")
-
-# Notifications send messages to the server, without response.
-#http_client.notify("add", 3, 4)
+if __name__ == "__main__":
+    main()
--- emotion_extractor/server.py
+++ emotion_extractor/server.py
@@ -1,20 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+from werkzeug.wrappers import Request, Response
+from werkzeug.serving import run_simple
 
-import pyjsonrpc
+from jsonrpc import JSONRPCResponseManager, dispatcher
 import emotion_extractor
 
-class RequestHandler(pyjsonrpc.HttpRequestHandler):
-	@pyjsonrpc.rpcmethod
-	def get_emotion(self, sentence):
-		return emotion_extractor.get_emotion(sentence)
-		
-# Threading HTTP-serveer
-http_server = pyjsonrpc.ThreadingHttpServer(
-    server_address = ('localhost', 10002),
-    RequestHandlerClass = RequestHandler
-)
+@dispatcher.add_method
+def get_emotion(*args, **kwargs):
+	print "get_emotion called"
+	return emotion_extractor.get_emotion(args[0])
 
-print "Starting HTTP server ..."
-print "URL: http://localhost:10002"
-http_server.serve_forever()
+@Request.application
+def application(request):
+    # Dispatcher is dictionary {<method_name>: callable}
+    dispatcher["echo"] = lambda s: s
+    dispatcher["add"] = lambda a, b: a + b
+
+    response = JSONRPCResponseManager.handle(
+        request.data, dispatcher)
+    return Response(response.json, mimetype='application/json')
+
+if __name__ == '__main__':
+    run_simple('143.248.142.86', 4000, application)
