401b00eb85fd7d5d7d12efaf3aecd8b55ab38ddd
JuHyeng Han <sparkly9399@kaist.ac.kr>
2015-11-13 18:04:02 +0900

    Make Twitter Global Object

--- TWIDDY_UI/src/com/example/twiddy_ui/TwitLogin.java
+++ TWIDDY_UI/src/com/example/twiddy_ui/TwitLogin.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,79 +22,56 @@ public class TwitLogin extends Activity {
 	
 	RequestToken requestToken;
     Twitter twitter;
+    Context ctx;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.twit_login);
-	
+		ctx = this;
 		LoadWebPageASYNC twitasync = new LoadWebPageASYNC();
 		twitasync.execute("");
     }
-	
-
 
 	@SuppressLint({ "SetJavaScriptEnabled", "JavascriptInterface" })
 	private class LoadWebPageASYNC extends AsyncTask<String, Void, String>{
 		
 		@Override
-		protected String doInBackground(String... urls){
-		
-	        ConfigurationBuilder cb = new ConfigurationBuilder();
-	         
-	        //the following is set without accesstoken- desktop client
-	        cb.setDebugEnabled(true)
-	      .setOAuthConsumerKey("6HE4SmIKhcf4uQa4a45nakhGW")
-	      .setOAuthConsumerSecret("p0kYyNJqgkq0NNc4RKkjmvNU4bapR0OJY1VqMmbJ9yMAgj1E5J");
-	   
-	        try {
-	            TwitterFactory tf = new TwitterFactory(cb.build());
-	            twitter = tf.getInstance();
-	            try {
-	                requestToken = twitter.getOAuthRequestToken(); 
-	                
-	                final String urltoload = requestToken.getAuthenticationURL();
+		protected String doInBackground(String... urls){		
+	        TwitterObj tobj = new TwitterObj();
+	        if(tobj.GetTwitter() == null){
+	        	tobj.SetTwitter();
+	        }
+	        twitter = tobj.GetTwitter();
+	        
+            try {
+                requestToken = twitter.getOAuthRequestToken(); 
+                
+                final String urltoload = requestToken.getAuthenticationURL();
 
-	                System.out.println("Open the following URL and grant access to your account:");
-	                System.out.println(requestToken.getAuthorizationURL());
-	                TwitLogin.this.runOnUiThread(new Runnable() {
-	                	public void run(){
-	                		final WebView webView = (WebView) findViewById(R.id.webView);
-	                		webView.getSettings().setJavaScriptEnabled(true);
-	                		webView.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
-	                		webView.loadUrl(urltoload);
-	                		webView.setWebViewClient(new WebViewClient() {
-	                            @Override
-	                            public void onPageFinished(WebView view, String url) {
-	                            	//webView.loadUrl("javascript:window.HTMLOUT.showHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
-	                            	webView.loadUrl("javascript:window.HTMLOUT.showHTML(document.getElementsByTagName('code')[0].innerHTML);");
-	                            	Log.d("HTML", "BBBBBBBBBB");
-	                            }
-	                        });
-	                	}
-	                });
-	                   
-	            } catch (IllegalStateException ie) {
-	                // access token is already available, or consumer key/secret is not set.
-	                if (!twitter.getAuthorization().isEnabled()) {
-	                    System.out.println("OAuth consumer key/secret is not set.");
-	                    System.exit(-1);
-	                }
-	            }
-	             
-	            //Status status = twitter.updateStatus("Hello From Twddy");
-	
+                System.out.println("Open the following URL and grant access to your account:");
+                System.out.println(requestToken.getAuthorizationURL());
+                TwitLogin.this.runOnUiThread(new Runnable() {
+                	public void run(){
+                		final WebView webView = (WebView) findViewById(R.id.webView);
+                		webView.getSettings().setJavaScriptEnabled(true);
+                		webView.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
+                		webView.loadUrl(urltoload);
+                		webView.setWebViewClient(new WebViewClient() {
+                            @Override
+                            public void onPageFinished(WebView view, String url) {
+                            	webView.loadUrl("javascript:window.HTMLOUT.showHTML("
+                            			+ "document.getElementsByTagName('code')[0].innerHTML);");	                            	
+                            }
+                        });
+                	}
+                });           
 	         } catch (TwitterException te) {
 	             te.printStackTrace();
-	             System.out.println("Failed to get timeline: " + te.getMessage());
+	             Log.d("HTML", "Failed to get timeline: " + te.getMessage());
 	             System.exit(-1);
-	         } 
-	        return null;
-		}
-		
-		@Override
-		protected void onPostExecute(String result){
-			
+	         }             
+            return null;
 		}
 		
 		class MyJavaScriptInterface  
@@ -101,28 +80,26 @@ public class TwitLogin extends Activity {
 		    public void showHTML(String pin) throws TwitterException  
 		    {  
 		    	if(pin.length() > 0 ) {
-		    		Log.d("HTML", "pin = " + pin);
-	                AccessToken accessToken = null;	                
-	                try {
-	                	if (pin.length() > 0) {
-	                		accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-	                		List<twitter4j.Status> statuses = twitter.getHomeTimeline();
-	                		Log.d("HTML","Showing home timeline.");
-	                	    for (twitter4j.Status status : statuses) {
-	                	        Log.d("HTML",status.getUser().getName() + ":" +
-	                	                           status.getText());
-	                	    }
-	                	} else {
-	                		accessToken = twitter.getOAuthAccessToken(requestToken);
-	                	}
+		    		Log.d("PIN", "pin = " + pin);             
+	                try {	                	
+	                	twitter.getOAuthAccessToken(requestToken, pin);
+	            		List<twitter4j.Status> statuses = twitter.getHomeTimeline();
+	            		Log.d("PIN","Showing home timeline.");
+	            	    for (twitter4j.Status status : statuses) {
+	            	        Log.d("PIN",status.getUser().getName() + ":" +
+	            	                           status.getText());
+	            	    }	         
+	            	    Intent it_display = new Intent(ctx, DisplayEmotion.class);
+	            	    it_display.putExtra("twitter", twitter);
+	        			startActivity(it_display);            			
+	            	    finish();
 	                } catch (TwitterException te) {
 	                	if (401 == te.getStatusCode()) {
 	                		System.out.println("Unable to get the access token.");
 	                	} else {
 	                		te.printStackTrace();
 	                	}
-	                }
-				
+	                }				
 		    	}
 		    	else {
 		    		Log.d("HTML", "No pin accept");
---
+++ TWIDDY_UI/src/com/example/twiddy_ui/TwitterObj.java
@@ -0,0 +1,31 @@
+package com.example.twiddy_ui;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterObj {
+	
+	private Twitter twitter;
+	
+	TwitterObj()
+	{
+		this.twitter = null;
+	}
+	
+	public Twitter GetTwitter(){
+		return this.twitter;	
+	}
+	
+	public void SetTwitter(){
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+	    
+	    //the following is set without accesstoken- desktop client
+	    cb.setDebugEnabled(true)
+	  .setOAuthConsumerKey("6HE4SmIKhcf4uQa4a45nakhGW")
+	  .setOAuthConsumerSecret("p0kYyNJqgkq0NNc4RKkjmvNU4bapR0OJY1VqMmbJ9yMAgj1E5J");
+	    
+	    TwitterFactory tf = new TwitterFactory(cb.build());
+		this.twitter = tf.getInstance();	
+	}
+}
