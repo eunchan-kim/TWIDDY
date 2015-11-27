b52ce3e5e28f0e36e679891b415c1179a393f629
JuHyeng Han <sparkly9399@kaist.ac.kr>
2015-11-27 16:24:02 +0900

    Bluetooth Complete

--- TWIDDY_UI/src/com/example/twiddy_ui/BluetoothService.java
+++ TWIDDY_UI/src/com/example/twiddy_ui/BluetoothService.java
@@ -1,71 +1,164 @@
 package com.example.twiddy_ui;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.os.Handler;
 import android.util.Log;
 
 public class BluetoothService {
-	// Debugging
-	private static final String TAG = "BluetoothService";
-	
-	// Intent request code
 	private static final int REQUEST_CONNECT_DEVICE = 1;
 	private static final int REQUEST_ENABLE_BT = 2;
 	
+	public static final int STATE_NONE = 0;
+	public static final int STATE_LISTEN = 1;
+	public static final int STATE_CONNECTING = 2;
+	public static final int STATE_CONNECTED = 3;
+	
+	private static final UUID MY_UUID = UUID
+			.fromString("00001101-0000-1000-8000-00805f9b34fb");
 	private BluetoothAdapter btAdapter;
-	
 	private Activity mActivity;
-	private Handler mHandler;
-	
-	// Constructors
-	public BluetoothService(Activity ac, Handler h) {
+	private ConnectThread mConnectThread;
+	private int mState;
+	private OutputStream mmOutStream;
+
+	public BluetoothService(Activity ac) {
 		mActivity = ac;
-		mHandler = h;
-		
-		// BluetoothAdapter 얻기
+		mState = STATE_NONE;
 		btAdapter = BluetoothAdapter.getDefaultAdapter();
 	}
-	
-	/**
-	 * Check the Bluetooth support
-	 * @return boolean
-	 */
+
 	public boolean getDeviceState() {
-		Log.i(TAG, "Check the Bluetooth support");
-		
-		if(btAdapter == null) {
-			Log.d(TAG, "Bluetooth is not available");
-			
+		if (btAdapter == null) {
 			return false;
-			
 		} else {
-			Log.d(TAG, "Bluetooth is available");
-			
 			return true;
 		}
 	}
-	
-	/**
-	 * Check the enabled Bluetooth
-	 */
+
 	public void enableBluetooth() {
-		Log.i(TAG, "Check the enabled Bluetooth");
-		
-		
-		if(btAdapter.isEnabled()) {		
-			// 기기의 블루투스 상태가 On인 경우
-			Log.d(TAG, "Bluetooth Enable Now");
-			
-			// Next Step
-		} else {		
-			// 기기의 블루투스 상태가 Off인 경우
-			Log.d(TAG, "Bluetooth Enable Request");
-			
+		if (btAdapter.isEnabled()) {
+			scanDevice();
+		} else {
 			Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			mActivity.startActivityForResult(i, REQUEST_ENABLE_BT);
 		}
 	}
-	
+
+	public void scanDevice() {
+		Intent serverIntent = new Intent(mActivity, DeviceListActivity.class);
+		mActivity.startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+	}
+
+	public void getDeviceInfo(Intent data) {
+		String address = data.getExtras().getString(
+				DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+		BluetoothDevice device = btAdapter.getRemoteDevice(address);
+		connect(device);
+	}
+
+	private synchronized void setState(int state) {
+		mState = state;
+	}
+
+	public synchronized int getState() {
+		return mState;
+	}
+
+	public synchronized void start() {
+		if (mConnectThread != null) {
+			mConnectThread.cancel();
+			mConnectThread = null;
+		}
+	}
+
+	public synchronized void connect(BluetoothDevice device) {
+		if (mState == STATE_CONNECTING) {
+			if (mConnectThread == null) {
+
+			} else {
+				mConnectThread.cancel();
+				mConnectThread = null;
+			}
+		}
+		mConnectThread = new ConnectThread(device);
+		mConnectThread.start();
+		setState(STATE_CONNECTING);
+	}
+
+	public synchronized void stop() {
+		if (mConnectThread != null) {
+			mConnectThread.cancel();
+			mConnectThread = null;
+		}
+		setState(STATE_NONE);
+	}
+
+	public void write(byte[] out) {
+		synchronized (this) {
+			if (mState != STATE_CONNECTED)
+				return;
+		}
+		try {
+			mmOutStream.write(out);
+
+		} catch (IOException e) {
+		}
+	}
+
+	// 연결 실패했을때
+	private void connectionFailed() {
+		setState(STATE_LISTEN);
+	}
+
+	private class ConnectThread extends Thread {
+		private final BluetoothSocket mmSocket;
+		
+		public ConnectThread(BluetoothDevice device) {
+			BluetoothSocket tmp = null;
+			try {
+				tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+			} catch (IOException e) {
+			}
+			mmSocket = tmp;			
+		}
+
+		public void run() {
+			btAdapter.cancelDiscovery();
+
+			try {
+				mmSocket.connect();
+				mmOutStream = mmSocket.getOutputStream();
+
+			} catch (IOException e) {
+				connectionFailed();
+				try {
+					mmSocket.close();
+				} catch (IOException e2) {
+				}
+				BluetoothService.this.start();
+				return;
+			}
+
+			synchronized (BluetoothService.this) {
+				mConnectThread = null;
+			}
+			
+			setState(STATE_CONNECTED);
+		}
+
+		public void cancel() {
+			try {
+				mmSocket.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
 }
